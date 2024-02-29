6f47b6b0f3e7acfa8427c209b8ec94e62acd024a
Mengyu Zhao <zhaomy8@asiainfo.com>
2024-02-29 15:12:20 +0800

    文章分类列表

--- src/main/java/com/itheima/service/CategoryService.java
+++ src/main/java/com/itheima/service/CategoryService.java
@@ -3,7 +3,11 @@ package com.itheima.service;
 import com.itheima.pojo.Category;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface CategoryService {
     //新建分类
     void add(Category category);
+//    列表查询
+    List<Category> list();
 }
