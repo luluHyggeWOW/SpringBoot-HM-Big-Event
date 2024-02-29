8d6abf1de0ef4d15d017679c26e5b61a4c9bfad3
Mengyu Zhao <zhaomy8@asiainfo.com>
2024-02-29 11:39:33 +0800

    更新用户头像

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -7,6 +7,7 @@ import com.itheima.utils.JwtUtil;
 import com.itheima.utils.Md5Util;
 import com.itheima.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -64,8 +65,13 @@ public class UserController {
         return Result.success(user);
     }
     @PutMapping("/update")
-    public Result update(@RequestBody User user){
+    public Result update(@RequestBody @Validated User user){
         userService.update(user);
         return  Result.success();
+    }
+    @PatchMapping("updateAvatar")
+    public Result updateAvatar(@RequestParam @URL String avatarUrl){
+        userService.updateAvatar(avatarUrl);
+        return Result.success();
     }
 }
--- src/main/java/com/itheima/service/UserService.java
+++ src/main/java/com/itheima/service/UserService.java
@@ -7,6 +7,8 @@ public interface UserService {
     User findByUserName(String username);
     //注册
     void register(String username, String password);
-
+//    更新用户信息
     void update(User user);
+//    更新头像;
+    void updateAvatar(String avatarUrl);
 }
